2ceee38e07f3dacde745e9cb0cb0957597b6fdf3
Mayank Sharma <59726829+sharmamayank77777@users.noreply.github.com>
2023-07-14 12:47:42 +0530

    Add files via upload

---
+++ wassup/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').createServer(app)
+const PORT = process.env.PORT || 3000
+
+
+http.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname + '/login.html')
+})
+
+
+
+
+//socket
+
+const io = require('socket.io')(http)
+io.on('connection',(socket) =>{
+    console.log('Connected....')
+    socket.on('message', (msg) =>{
+        socket.broadcast.emit('message', msg)
+    })
+})
